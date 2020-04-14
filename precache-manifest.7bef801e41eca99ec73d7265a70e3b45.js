2d73aea38333eb6acd8d881da0a2c7f6bc0b78c7
Jesse Russell <jrussellfreelance@gmail.com>
2020-04-14 13:00:16 -0400

    Updates

---
+++ precache-manifest.7bef801e41eca99ec73d7265a70e3b45.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b0cc95af814faecba0da58469278a45",
+    "url": "/jrussell-md/index.html"
+  },
+  {
+    "revision": "6cf73f662ef625f89908",
+    "url": "/jrussell-md/static/css/2.7d2e3730.chunk.css"
+  },
+  {
+    "revision": "402ec6782f517c0c65a0",
+    "url": "/jrussell-md/static/css/main.c0fe06a2.chunk.css"
+  },
+  {
+    "revision": "6cf73f662ef625f89908",
+    "url": "/jrussell-md/static/js/2.a1a9fd4a.chunk.js"
+  },
+  {
+    "revision": "0a8b59af9bc84e5a7421b6d864f5a504",
+    "url": "/jrussell-md/static/js/2.a1a9fd4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "402ec6782f517c0c65a0",
+    "url": "/jrussell-md/static/js/main.a6e33958.chunk.js"
+  },
+  {
+    "revision": "e6a757c1b19cd99c8a29",
+    "url": "/jrussell-md/static/js/runtime-main.d1728c26.js"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "/jrussell-md/static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "/jrussell-md/static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "/jrussell-md/static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "/jrussell-md/static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "/jrussell-md/static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "/jrussell-md/static/media/icons-20.cef8cdbb.woff"
+  }
+]);
